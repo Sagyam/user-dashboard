650ef6e3ecd67edb8c03f0cbeaa50a6cb16e4c72
Sagyam Thapa <sagyamthapa32@gmail.com>
2022-06-09 14:46:24 +0545

    Responsive

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -11,7 +11,7 @@ function UserList() {
 	}, []);
 
 	return (
-		<div className="grid grid-cols-4 gap-y-8 gap-x-10 m-10 ">
+		<div className="grid 2xl:grid-cols-6 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-y-8 gap-x-10 m-10 ">
 			{users.map((user) => (
 				<Card key={user.id} user={user} />
 			))}
